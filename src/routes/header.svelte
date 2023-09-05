aa4aa59d553e74863f5db394f521e1b8fb5be196
marco <marcoeribes@gmail.com>
2023-09-05 12:01:16 -0700

    added theme changer

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,52 +1,52 @@
 <script>
-    import * as config from '$lib/config'
+	import * as config from '$lib/config';
+	import Toggle from './toggle.svelte';
 </script>
 
 <nav>
-    <a href="/" class="title">
-        <b>{config.title}</b>
-    </a>
+	<a href="/" class="title">
+		<b>{config.title}</b>
+	</a>
 
-    <ul class="links">
-        <li>
-            <a href="/about">About</a>
-        </li>
-        <li>
-            <a href="/contact">Contact</a>
-        </li>
-        <li>
-            <a href="/rss.xml" target="_blank">RSS</a>
-        </li>
-        
-    </ul>
+	<ul class="links">
+		<li>
+			<a href="/about">About</a>
+		</li>
+		<li>
+			<a href="/contact">Contact</a>
+		</li>
+		<li>
+			<a href="/rss.xml" target="_blank">RSS</a>
+		</li>
+	</ul>
 
-    <button>Toggle</button>
+	<Toggle />
 </nav>
 
 <style>
-    nav {
-        padding-block: var(--size-7);
-    }
+	nav {
+		padding-block: var(--size-7);
+	}
 
-    .links {
-        margin-block: var(--size-7);
-    }
+	.links {
+		margin-block: var(--size-7);
+	}
 
-    a {
-        color: inherit;
-        text-decoration: none;
-    }
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
 
-    @media (min-width: 768px) {
-        nav {
-            display: flex;
-            justify-content: space-between;
-        }
+	@media (min-width: 768px) {
+		nav {
+			display: flex;
+			justify-content: space-between;
+		}
 
-        .links {
-            display: flex;
-            gap: var(--size-7);
-            margin-block: 0;
-        }
-    }
+		.links {
+			display: flex;
+			gap: var(--size-7);
+			margin-block: 0;
+		}
+	}
 </style>
